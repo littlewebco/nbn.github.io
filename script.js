818edccaca445f3ccb39e7dec5588e0bc4424082
linq <royal@perennial-tech.com>
2022-08-23 15:45:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function handleKeyDown() {
     if (event.key === 'Enter') {
-      location.href = "https://impervious.domains/search/" + event.target.value;
+      location.href = "https://nbn-service-check.deta.dev/check?address=" + event.target.value;
     }
   };
 
